e10c9d0ca7e8de2cdb420bcabd9661c34c14cf5b
Дмитрий <craizer18@mail.ru>
2023-11-25 16:12:53 +0700

    Задание 1

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
-import uvicorn
+#import uvicorn
 app= FastAPI()
 
 @app.get('/')
 async def f_index():
-     return {"message": "World","keyroot":"Привет!"}
+     return {"Терехин " "Дмитри ""Валерьевич"}
 
 @app.get('/tools')
 async def f_indexT():
-     return {"message":"Tols","key": "Привет"}
+     return {"Разработчик на питоне"}
 
-if __name__ =='__main__':
-     uvicorn.run(app= "main:app",host = "127.0.0.1", port=600)
+@app.get('/users')
+async def f_index1():
+     return "88005553535"
+#if __name__ =='__main__':
+    # uvicorn.run(app= "main:app",host = "127.0.0.1", port=600)
